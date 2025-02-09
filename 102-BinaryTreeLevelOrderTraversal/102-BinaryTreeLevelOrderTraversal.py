3cecfd2a33929f7b67d4e4079ac250b513f69869
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-02-08 21:54:25 -0500

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,25 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        if not root:
+            return res
+        
+        q = collections.deque()
+        q.append(root)
+    
+        while q:
+            same_level = []
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                same_level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            res.append(same_level)
+        
+        return res
