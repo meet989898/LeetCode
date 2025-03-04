4509e754cf4699def45f68901a113dd155c3ffe5
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-03-03 23:23:58 -0500

    Create file

---
+++ 1780-CheckifNumberisaSumofPowersofThree/1780-CheckifNumberisaSumofPowersofThree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            # Check if this power should be used twice
+            if n % 3 == 2:
+                return False
+            # Divide n by 3 to move to the next greater power
+            n //= 3
+        # The ternary representation of n consists only of 0s and 1s
+        return True
