d38ff1b509c3f32c6aa6b3772fc9f38bb6be77ee
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-04-01 23:34:09 -0400

    Create file

---
+++ 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.py
@@ -0,0 +1,13 @@
+# Last updated: 4/1/2025, 11:34:09 PM
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1])
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i])
+        res = 0
+        for j in range(1, n - 1):
+            res = max(res, (leftMax[j] - nums[j]) * rightMax[j])
+        return res
