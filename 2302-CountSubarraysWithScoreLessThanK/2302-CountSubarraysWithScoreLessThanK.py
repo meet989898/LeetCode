bf1ff0809992dc09226aa57d0ad4ed5e1b43292c
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-04-27 23:09:41 -0400

    Create file

---
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.py
@@ -0,0 +1,13 @@
+# Last updated: 4/27/2025, 11:09:40 PM
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        res, total = 0, 0
+        i = 0
+        for j in range(n):
+            total += nums[j]
+            while i <= j and total * (j - i + 1) >= k:
+                total -= nums[i]
+                i += 1
+            res += j - i + 1
+        return res
