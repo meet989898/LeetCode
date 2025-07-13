8c69d051f937ff9e1cd82550b38c648ab956fc78
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-07-12 22:56:54 -0400

    Create 2410-MaximumMatchingofPlayersWithTrainers

---
+++ 2410-MaximumMatchingofPlayersWithTrainers/2410-MaximumMatchingofPlayersWithTrainers.py
@@ -0,0 +1,17 @@
+# Last updated: 7/12/2025, 10:56:54 PM
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        m, n = len(players), len(trainers)
+        i = j = count = 0
+
+        while i < m and j < n:
+            while j < n and players[i] > trainers[j]:
+                j += 1
+            if j < n:
+                count += 1
+            i += 1
+            j += 1
+
+        return count
