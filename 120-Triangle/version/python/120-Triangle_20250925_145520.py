8e119a9cc5a452060166d4b9eac5713605070622
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-09-25 14:55:20 -0400

    Create 120-Triangle

---
+++ 120-Triangle/version/python/120-Triangle_20250925_145520.py
@@ -0,0 +1,7 @@
+# Last updated: 9/25/2025, 2:55:20 PM
+class Solution:
+    def minimumTotal(self, tri: List[List[int]]) -> int:
+        for i in range(len(tri) - 2, -1, -1):
+            for j in range(len(tri[i])):
+                tri[i][j] += min(tri[i + 1][j], tri[i + 1][j + 1])
+        return tri[0][0]
