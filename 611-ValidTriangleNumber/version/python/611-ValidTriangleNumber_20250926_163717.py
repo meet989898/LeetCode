fb24e65429b0f43c2b49d47e53549fc4aac56d84
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-09-26 16:37:16 -0400

    Create 611-ValidTriangleNumber

---
+++ 611-ValidTriangleNumber/version/python/611-ValidTriangleNumber_20250926_163717.py
@@ -0,0 +1,16 @@
+# Last updated: 9/26/2025, 4:37:17 PM
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        count = 0
+
+        for i in range(n - 1, -1, -1):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return count
