d94045c9a1d6c53628832170e27c7a7c1c99b29f
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-01 09:55:48 -0400

    Create 1518-WaterBottles

---
+++ 1518-WaterBottles/version/python/1518-WaterBottles_20251001_095549.py
@@ -0,0 +1,16 @@
+# Last updated: 10/1/2025, 9:55:49 AM
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+
+        consumed_bottles = 0
+
+        while numBottles >= numExchange:
+            # Consume numExchange full bottles.
+            consumed_bottles += numExchange
+            numBottles -= numExchange
+
+            # Exchange them for one full bottle.
+            numBottles += 1
+
+        # Consume the remaining numBottles (less than numExchange).
+        return consumed_bottles + numBottles
