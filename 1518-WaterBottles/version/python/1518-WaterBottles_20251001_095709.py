11f7b3ab7fac92cfdf36f98340813fc8f4ac173a
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-01 09:57:09 -0400

    Create 1518-WaterBottles

---
+++ 1518-WaterBottles/version/python/1518-WaterBottles_20251001_095709.py
@@ -0,0 +1,17 @@
+# Last updated: 10/1/2025, 9:57:09 AM
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+
+        consumed_bottles = 0
+
+        while numBottles >= numExchange:
+            # Maximum number of times we can consume numExchange
+            # number of bottles using numBottles.
+            K = numBottles // numExchange
+
+            consumed_bottles += numExchange * K
+            numBottles -= numExchange * K
+
+            numBottles += K
+
+        return consumed_bottles + numBottles
