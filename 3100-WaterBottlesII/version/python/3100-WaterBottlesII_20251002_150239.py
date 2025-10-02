bff648615b35f0faa3bd5bc3909b4e291d1d7528
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-02 15:02:39 -0400

    Create 3100-WaterBottlesII

---
+++ 3100-WaterBottlesII/version/python/3100-WaterBottlesII_20251002_150239.py
@@ -0,0 +1,10 @@
+# Last updated: 10/2/2025, 3:02:39 PM
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        empty = numBottles
+        while empty >= numExchange:
+            ans += 1
+            empty -= numExchange - 1
+            numExchange += 1
+        return ans
