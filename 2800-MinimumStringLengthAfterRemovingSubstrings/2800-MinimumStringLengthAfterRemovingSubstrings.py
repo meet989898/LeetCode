1f410249cea4b0d6998c5b7bcaf615ce21126d62
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-07 10:28:02 -0400

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.py
@@ -0,0 +1,19 @@
+# Last updated: 10/7/2025, 10:28:02 AM
+class Solution:
+    def minLength(self, s: str) -> int:
+        
+        stack = []
+
+        for c in s:
+            if not stack:
+                stack.append(c)
+                continue
+            if c == "B" and stack[-1] == "A":
+                stack.pop()
+            elif c== "D" and stack[-1] == "C":
+                stack.pop()
+            else:
+                stack.append(c)
+
+        return len(stack)
+        
