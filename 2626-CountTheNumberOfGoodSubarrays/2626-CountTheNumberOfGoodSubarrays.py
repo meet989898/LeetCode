c15486beffed3ddc26dbd23140c993820dd08df5
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-07 10:28:18 -0400

    Create 2626-CountTheNumberOfGoodSubarrays

---
+++ 2626-CountTheNumberOfGoodSubarrays/2626-CountTheNumberOfGoodSubarrays.py
@@ -0,0 +1,17 @@
+# Last updated: 10/7/2025, 10:28:18 AM
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        same, right = 0, -1
+        cnt = Counter()
+        ans = 0
+        for left in range(n):
+            while same < k and right + 1 < n:
+                right += 1
+                same += cnt[nums[right]]
+                cnt[nums[right]] += 1
+            if same >= k:
+                ans += n - right
+            cnt[nums[left]] -= 1
+            same -= cnt[nums[left]]
+        return ans
