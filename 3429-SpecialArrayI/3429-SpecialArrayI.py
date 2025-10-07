92f9bb5fecfccf47334d21b7228cf8d1e5fa7216
Meet Gandhi <73106383+meet989898@users.noreply.github.com>
2025-10-07 10:27:32 -0400

    Create 3429-SpecialArrayI

---
+++ 3429-SpecialArrayI/3429-SpecialArrayI.py
@@ -0,0 +1,14 @@
+# Last updated: 10/7/2025, 10:27:31 AM
+class Solution:
+    def isArraySpecial(self, nums):
+        # Iterate through indexes 0 to n - 1
+        for index in range(len(nums) - 1):
+
+            # Compare the parities of the current and next number
+            if nums[index] % 2 == nums[index + 1] % 2:
+
+                # If the two adjacent numbers have the same parity, return False
+                return False
+
+        # Return True if no pair of adjacent numbers with the same parity are found
+        return True
